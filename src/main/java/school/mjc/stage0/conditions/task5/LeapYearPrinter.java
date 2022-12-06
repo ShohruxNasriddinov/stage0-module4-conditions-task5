518858234c858f1fe285f371e5046ad47c493f9d
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-06 11:49:35 +0500

    commit task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int a = year%100;
+        int b = year%4;
+        switch (b){
+            case 0:
+                System.out.println("leap");
+            default:
+                System.out.println("not leap");
+        }
     }
 }
